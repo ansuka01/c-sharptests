c6b52f0469bbc685ff2850bee550077b2b12919f
Atte Sukari <87363956+ansuka01@users.noreply.github.com>
2023-12-27 13:24:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TeleprompterConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string? line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
